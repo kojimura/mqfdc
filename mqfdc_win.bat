a7ba1aaa121ff075d6cd50cf8bc66f9231a5d83b
Kouji Murakami <kojimura@gmail.com>
2018-06-15 20:00:58 +0900

    windows batch script

---
+++ mqfdc_win.bat
@@ -0,0 +1,13 @@
+rem @echo off
+
+rem Simplify all MQ FDC files in a specified folder
+rem usage: mqfdfc full-path (if no argument then look currect directory)
+
+set TARGETDIR=
+if not "%~1"=="" (
+ set TARGETDIR=%1\
+)
+
+for /f "usebackq" %%i in (`dir /B %TARGETDIR%*.FDC`) do (
+  mqfdc < %TARGETDIR%%%i
+)
