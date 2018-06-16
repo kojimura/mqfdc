b8bb313895c95d283b8287e171b29a081720dd8f
Kouji Murakami <kojimura@gmail.com>
2018-06-16 12:18:33 +0900

    change power shell and cygwin shell for Windows

---
+++ mqfdc.ps1
@@ -0,0 +1,10 @@
+<#
+Simplify all MQ FDC files in spesified directory
+usage: .\mqfdcawk.ps1 /var/mqm/errors/
+#>
+Param( $p1 )
+$f = Get-ChildItem $p1 -Filter *.FDC
+foreach ($i in $f){
+echo $i.Name
+cat $i.FullName | %{ if($_.Contains("Probe Id") -Or $_.Contains("Date/Time") -Or ($_.Split(' ')[1] -eq "Component") -Or $_.Contains("Process Name") -Or $_.Contains("Major Errorcode")) { $_; } }
+}
---
+++ mqfdc_win.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simplify all MQ FDC files in spesified directory
+# usage: mqfdcawk.sh /var/mqm/errors/
+
+fdcPath=$1
+
+for filename in ${fdcPath}*.FDC; do
+ echo "${filename}"
+ awk '{if($2=="Date/Time" || $2=="Probe" && $3=="Id" || $2=="Component" || $2=="Process" && $3=="Name" || $2=="Major" && $3=="Errorcode") print }' < ${filename}
+done
